0efffb517bd6216afb4d17d690e7a74d2b3ba1b4
cristopxer <cristopherortiz457@gmail.com>
2022-12-08 20:30:44 -0600

    Update posts file typo firstname on createPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,7 +7,7 @@ export const createPost = async (req, res) => {
     const user = await User.findById(userId);
     const newPost = new Post({
       userId,
-      firstaName: user.firstName,
+      firstName: user.firstName,
       lastName: user.lastName,
       location: user.location,
       description,
