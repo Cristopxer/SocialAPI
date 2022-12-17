fe2b9947e92eb89974adbc63bf57afd63becd769
cristopxer <cristopherortiz457@gmail.com>
2022-12-17 15:32:23 -0600

    return user from login response

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,16 +26,16 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
+    var user = await User.findOne({ email: email });
     if (!user) return res.status(400).json({ msg: "User does not exist" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalidad credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const userFormatted = user.toJSON();
-    delete userFormatted.password;    
-    return res.status(200).json({ token, userFormatted });
+    user = user.toJSON();
+    delete user.password;    
+    return res.status(200).json({ token, user });
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
