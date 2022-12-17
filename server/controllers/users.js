08dabab3ab8f620d2a6d865d4bfcefd4206b9c06
cristopxer <cristopherortiz457@gmail.com>
2022-12-17 16:12:33 -0600

    return updated friends on AddRemoveFriends control

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -34,24 +34,25 @@ export const getUserFriends = async (req, res) => {
 
 export const addRemoveFriend = async (req, res) => {
   try {
-    const { id, frinedId } = req.params;
+    const { id, friendId } = req.params;
     const user = await User.findById(id);
-    const friend = await User.findById(frinedId);
-    if (user.friends.includes(frinedId)) {
-      user.friends = user.friends.filter((id) => id !== frinedId);
+    const friend = await User.findById(friendId);
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
     } else {
-      user.friends = user.friends.push(frinedId);
-      friend.friends = friend.friends.push(id);
+      user.friends.push(friendId);
+      friend.friends.push(id);
     }
     await user.save();
     await friend.save();
 
-    const friends = Promise.all(
-      user.friends.map((id) => {
-        User.findById(id);
+    const friends = await Promise.all(
+      user.friends.map((fId) => {        
+        return User.findById(fId);
       })
     );
+    
     const formatedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
